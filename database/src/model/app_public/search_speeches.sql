3346a156fd7844b5dfd7b0d6f245451552277792
Jakob Kraus <jakob.kraus@limebit.de>
2020-10-14 17:27:33 +0200

    OD-3 Updated database function to return first_name, last_name and faction abbreviation

--- database/src/model/app_public/search_speeches.sql
+++ database/src/model/app_public/search_speeches.sql
@@ -1,18 +1,19 @@
-CREATE OR REPLACE FUNCTION app_public.search_speeches (politician_id_query int8 DEFAULT -2, faction_id_query int8 DEFAULT -2, "position_short_query" text DEFAULT '', content_query text DEFAULT '', from_date date DEFAULT NULL, to_date date DEFAULT NULL)
+CREATE OR REPLACE FUNCTION app_public.search_speeches (politician_id_query int8 DEFAULT -1, faction_id_query int8 DEFAULT -1, "position_short_query" text DEFAULT '', content_query text DEFAULT '', from_date date DEFAULT NULL, to_date date DEFAULT NULL)
   RETURNS TABLE (
     id int8,
-    first_name varchar,
-    last_name varchar,
     "position_short" varchar,
     "date" date,
     speech_content text,
     document_url varchar,
-    rank float4
+    rank float4,
+    first_name varchar,
+    last_name varchar,
+    abbreviation varchar
   )
   AS $$
 DECLARE
-  has_politician_id boolean = politician_id_query > -2;
-  DECLARE has_faction_id boolean = faction_id_query > -2;
+  has_politician_id boolean = politician_id_query > -1;
+  DECLARE has_faction_id boolean = faction_id_query > -1;
   DECLARE has_content boolean = length(TRIM(content_query)) > 0;
   DECLARE has_position_short boolean = length(TRIM(position_short_query)) > 0;
   DECLARE position_short_tsquery tsquery = websearch_to_tsquery ('simple', position_short_query);
@@ -21,15 +22,18 @@ BEGIN
   RETURN QUERY
   SELECT
     s.id,
-    s.first_name,
-    s.last_name,
     s.position_short,
     s.date,
     s.speech_content,
     s.document_url,
-    ts_rank(search_speech_content, content_tsquery) + ts_rank(search_position_short, position_short_tsquery) AS rank
+    ts_rank(search_speech_content, content_tsquery) + ts_rank(search_position_short, position_short_tsquery) AS rank,
+    p.first_name,
+    p.last_name,
+    f.abbreviation
   FROM
     app_public.speeches s
+  LEFT JOIN app_public.politicians p ON p.id=s.politician_id
+  LEFT JOIN app_public.factions f ON f.id=s.faction_id
   WHERE (
     CASE WHEN has_politician_id
       AND NOT has_faction_id
