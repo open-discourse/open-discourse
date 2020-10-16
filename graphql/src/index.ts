ee41b4419bcd07c0946ee29138a738c9b4bef904
Jakob Kraus <jakob.kraus@limebit.de>
2020-10-16 17:08:54 +0200

    OD-7 Small changes to Dockerfile

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -14,22 +14,28 @@ app.use([
       user: process.env.POSTGRES_DB_USER,
       password: process.env.POSTGRES_DB_PASSWORD,
       host: process.env.POSTGRES_DB_HOST,
-      port: parseInt(process.env.POSTGRES_DB_PORT || "5000", 10)
+      port: parseInt(process.env.POSTGRES_DB_PORT || "5432", 10),
     },
     SCHEMA_PUBLIC,
     {
       appendPlugins: [PgSimplifyInflectorPlugin, ConnectionFilterPlugin],
       graphileBuildOptions: {
-        pgOmitListSuffix: true
+        pgOmitListSuffix: true,
       },
       enableCors: true,
       watchPg: true,
       simpleCollections: "only",
       graphiql: true,
       enhanceGraphiql: true,
-      dynamicJson: true
+      dynamicJson: true,
     }
-  )
+  ),
 ]);
 
-app.listen(5000, "0.0.0.0");
+app.listen(process.env.PORT || 8080, () =>
+  console.log(
+    "\x1b[33m%s\x1b[0m",
+    ">> server is ready, listening on port",
+    process.env.PORT || 8080
+  )
+);
