da25e66dcb28fc087c68895c694dd5bddc73806e
Jakob Kraus <jakob.kraus@limebit.de>
2020-11-06 11:09:38 +0100

    #3 Extracted Download Button Component

---
+++ frontend/src/components/download-button.tsx
@@ -0,0 +1,37 @@
+import { SearchResultRow } from "./hooks/use-manage-data";
+import { Button } from "@chakra-ui/core";
+
+export interface DownloadButtonProps {
+  data: SearchResultRow[];
+  text: string;
+}
+
+export const DownloadButton = ({ data, text }: DownloadButtonProps) => (
+  <a
+    style={{ margin: "5px" }}
+    download={"test.csv"}
+    href={`data:text/csv;base64,${btoa(
+      unescape(
+        encodeURIComponent(
+          "id,firstname,lastname,faction,position,date, url,speech\n" +
+            data
+              .map(
+                (element) =>
+                  `${JSON.stringify(element.id)},${JSON.stringify(
+                    JSON.stringify(element.firstName)
+                  )},${JSON.stringify(element.lastName)},${JSON.stringify(
+                    element.abbreviation
+                  )},${JSON.stringify(element.positionShort)},${JSON.stringify(
+                    element.date
+                  )},${JSON.stringify(element.documentUrl)},${JSON.stringify(
+                    element.speechContent
+                  )}\n`
+              )
+              .reduce((a, b) => a + b, "")
+        )
+      )
+    )}`}
+  >
+    <Button colorScheme="teal">{text}</Button>
+  </a>
+);
--- frontend/src/components/result-table.tsx
+++ frontend/src/components/result-table.tsx
@@ -17,6 +17,7 @@ import { ReactTable } from "@bit/limebit.chakra-ui-recipes.react-table";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import React, { useState } from "react";
 import { SearchResultRow } from "./hooks/use-manage-data";
+import { DownloadButton } from "./download-button";
 
 interface ResultTableProps {
   data: SearchResultRow[];
@@ -149,69 +150,17 @@ export const ResultTable = ({ data }: ResultTableProps) => {
     <>
       <ReactTable columns={columns} data={data} pageSize={10} />
       <Flex>
-        <a
-          download={"test.csv"}
-          href={`data:text/csv;base64,${btoa(
-            unescape(
-              encodeURIComponent(
-                "id,firstname,lastname,faction,position,date, url,speech\n" +
-                  data
-                    .map(
-                      (element) =>
-                        `${JSON.stringify(element.id)},${JSON.stringify(
-                          JSON.stringify(element.firstName)
-                        )},${JSON.stringify(element.lastName)},${JSON.stringify(
-                          element.abbreviation
-                        )},${JSON.stringify(
-                          element.positionShort
-                        )},${JSON.stringify(element.date)},${JSON.stringify(
-                          element.documentUrl
-                        )},${JSON.stringify(element.speechContent)}\n`
-                    )
-                    .reduce((a, b) => a + b, "")
-              )
-            )
-          )}`}
-        >
-          <Button colorScheme="teal">Download All</Button>
-        </a>
+        <DownloadButton data={data} text={"Download All"} />
         {selected.filter((element) => element.state == true).length > 0 ? (
-          <a
-            style={{ marginLeft: "10px" }}
-            download={"test.csv"}
-            href={`data:text/csv;base64,${btoa(
-              unescape(
-                encodeURIComponent(
-                  "id,firstname,lastname,faction,position,date, url,speech\n" +
-                    data
-                      .filter(
-                        (element) =>
-                          selected.find(
-                            (selectedElement) =>
-                              selectedElement.id == element.downloadId
-                          )?.state == true
-                      )
-                      .map(
-                        (element) =>
-                          `${JSON.stringify(element.id)},${JSON.stringify(
-                            JSON.stringify(element.firstName)
-                          )},${JSON.stringify(
-                            element.lastName
-                          )},${JSON.stringify(
-                            element.abbreviation
-                          )},${JSON.stringify(
-                            element.positionShort
-                          )},${JSON.stringify(element.date)},${JSON.stringify(
-                            element.documentUrl
-                          )},${JSON.stringify(element.speechContent)}\n`
-                      )
-                      .reduce((a, b) => a + b, "")
-                )
-              )
-            )}`}
-          >
-            <Button colorScheme="teal">Download Selected</Button>
-          </a>
+          <DownloadButton
+            data={data.filter(
+              (element) =>
+                selected.find(
+                  (selectedElement) => selectedElement.id == element.downloadId
+                )?.state == true
+            )}
+            text={"Download Selected"}
+          />
         ) : null}
       </Flex>
     </>
