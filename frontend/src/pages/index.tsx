a745e2ce3e3145beb0e81182847341f0e1bbf5bc
Jakob Kraus <jakob.kraus@limebit.de>
2020-12-02 13:18:56 +0100

    Revert "#3 switched to stable version of chakra ui 1.0"
    This reverts commit 0003d53782af828dc155c2d31c4638b89bfe4615.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { BaseTemplate } from "../templates/base-template";
-import { Heading, Flex, Stack } from "@chakra-ui/react";
+import { Heading, Flex, Stack } from "@chakra-ui/core";
 import { SearchForm } from "../components/search-form";
 import { SearchResult } from "../components/search-result";
 export interface QueryParams {
