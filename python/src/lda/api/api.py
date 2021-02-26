6b3f9784365801caa85cd6f71f73d1d92e347e67
Jabob Krauskopf <jakob.kraus10@gmail.com>
2021-02-26 17:36:01 +0100

    #64 Added dimension merging

--- python/src/lda/api/api.py
+++ python/src/lda/api/api.py
@@ -16,22 +16,26 @@ def get_dims():
 def query_data(args):
     dims = get_dims()
     keys = list(dims.keys())
-    cache = [
-        "INNER JOIN lda.{0} ON lda.{0}.id=lda.{1}.{0}_id_{2}".format(ne, la, args[ne])
-        for ne, la in zip(keys[1:-1], keys[:-2])
-    ]
-    cache.append(
-        "INNER JOIN lda.{0} ON ({1})".format(
-            keys[-1],
-            " OR ".join(
-                [
-                    "lda.{0}.id=lda.{1}.{0}_id_{2}".format(keys[-1], keys[-2], x)
-                    for x in range(dims[keys[-1]])
-                ]
-            ),
-        )
-    )
-    query = "SELECT lda.{0}.value FROM lda.{1} {2} WHERE lda.{1}.id={3}".format(
+    cache = []
+    format_string = "INNER JOIN lda.{0} ON {1}"
+    for next_key, last_key in zip(keys[1:], keys[:-1]):
+        if next_key in args:
+            string = "lda.{0}.id=lda.{1}.{0}_id_{2}".format(
+                next_key, last_key, args[next_key]
+            )
+        else:
+            string = (
+                "("
+                + " OR ".join(
+                    [
+                        "lda.{0}.id=lda.{1}.{0}_id_{2}".format(next_key, last_key, x)
+                        for x in range(dims[next_key])
+                    ]
+                )
+                + ")"
+            )
+        cache.append(format_string.format(next_key, string))
+    query = "SELECT lda.{0}.value, lda.{0}.n FROM lda.{1} {2} WHERE lda.{1}.id={3}".format(
         keys[-1], keys[0], " ".join(cache), args[keys[0]]
     )
 
@@ -41,7 +45,18 @@ def query_data(args):
         cur.execute("ROLLBACK")
         connection.commit()
         cur.execute(query)
-    return [0 if np.isnan(x[0]) else x[0] for x in cur.fetchall()]
+    data = [(0, x[1]) if np.isnan(x[0]) else (x[0], x[1]) for x in cur.fetchall()]
+    squashed_data = []
+    for data_points in [data[i :: dims[keys[-1]]] for i in range(dims[keys[-1]])]:
+        combined_weight = sum([data_point[1] for data_point in data_points])
+        median = (
+            0
+            if combined_weight == 0
+            else sum([data_point[0] * data_point[1] for data_point in data_points])
+            / combined_weight
+        )
+        squashed_data.append(median)
+    return squashed_data
 
 
 app = Flask(__name__)
