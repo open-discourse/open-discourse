d2522b31c3bc881e82492adff4dc7ae3736000ec
Jabob Krauskopf <jakob.kraus10@gmail.com>
2021-03-07 14:41:06 +0100

    #64 added multi schema and key support to api

--- python/src/lda/api/api.py
+++ python/src/lda/api/api.py
@@ -6,9 +6,49 @@ import numpy as np
 import psycopg2
 import os
 
+constraints = None
+dims = None
 
-def get_dims(connection, cursor):
-    dims_query = "SELECT table_name, n FROM lda.dims"
+
+def get_constraints(schema, connection, cursor):
+    global constraints
+    query = """
+        SELECT
+            ccu.table_name,
+            kcu.column_name
+        FROM
+            information_schema.table_constraints AS tc
+            JOIN information_schema.key_column_usage AS kcu
+            ON tc.constraint_name = kcu.constraint_name
+            AND tc.table_schema = kcu.table_schema
+            JOIN information_schema.constraint_column_usage AS ccu
+            ON ccu.constraint_name = tc.constraint_name
+            AND ccu.table_schema = tc.table_schema
+        WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_schema='{0}';
+        """.format(
+        schema
+    )
+    try:
+        cursor.execute(query)
+    except psycopg2.errors.InFailedSqlTransaction:
+        cursor.execute("ROLLBACK")
+        connection.commit()
+        cursor.execute(query)
+    if constraints:
+        constraints[schema] = {}
+    else:
+        constraints = {schema: {}}
+    for res in cursor.fetchall():
+        if res[0] in constraints[schema]:
+            constraints[schema][res[0]].append(res[1])
+        else:
+            constraints[schema][res[0]] = [res[1]]
+    return constraints
+
+
+def get_dims(schema, connection, cursor):
+    global dims
+    dims_query = "SELECT table_name, n FROM {0}.dims".format(schema)
     try:
         cursor.execute(dims_query)
     except psycopg2.errors.InFailedSqlTransaction:
@@ -16,7 +56,11 @@ def get_dims(connection, cursor):
         connection.commit()
         cursor.execute(dims_query)
     res = cursor.fetchall()
-    return {x[0]: x[1] for x in res}
+    if dims:
+        dims[schema] = {x[0]: x[1] for x in res}
+    else:
+        dims = {schema: {x[0]: x[1] for x in res}}
+    return dims
 
 
 def get_db():
@@ -25,30 +69,39 @@ def get_db():
     return g.db
 
 
-def query_data(args, connection, cursor):
-    dims = get_dims(connection, cursor)
-    keys = list(dims.keys())
+def query_data(args, schema, connection, cursor):
+    dim = (
+        dims[schema]
+        if dims and schema in dims
+        else get_dims(schema, connection, cursor)[schema]
+    )
+    keys = list(dim.keys())
     cache = []
-    format_string = "INNER JOIN lda.{0} ON {1}"
+    format_string = "INNER JOIN " + schema + ".{0} ON {1}"
     for next_key, last_key in zip(keys[1:], keys[:-1]):
         if next_key in args:
-            string = "lda.{0}.id=lda.{1}.{0}_id_{2}".format(
-                next_key, last_key, args[next_key]
+            string = "{0}.{1}.id={0}.{2}.{3}".format(
+                schema, next_key, last_key, args[next_key]
             )
         else:
+            constraint = (
+                constraints[schema][next_key]
+                if constraints and schema in constraints
+                else get_constraints(schema, connection, cursor)[schema][next_key]
+            )
             string = (
                 "("
                 + " OR ".join(
                     [
-                        "lda.{0}.id=lda.{1}.{0}_id_{2}".format(next_key, last_key, x)
-                        for x in range(dims[next_key])
+                        "{0}.{1}.id={0}.{2}.{3}".format(schema, next_key, last_key, x)
+                        for x in constraint
                     ]
                 )
                 + ")"
             )
         cache.append(format_string.format(next_key, string))
-    query = "SELECT lda.{0}.value, lda.{0}.n FROM lda.{1} {2} WHERE lda.{1}.id={3}".format(
-        keys[-1], keys[0], " ".join(cache), args[keys[0]]
+    query = "SELECT {0}.{1}.value, {0}.{1}.n FROM {0}.{2} {3} WHERE {0}.{2}.id='{4}'".format(
+        schema, keys[-1], keys[0], " ".join(cache), args[keys[0]]
     )
 
     try:
@@ -59,7 +112,7 @@ def query_data(args, connection, cursor):
         cursor.execute(query)
     data = [(0, x[1]) if np.isnan(x[0]) else (x[0], x[1]) for x in cursor.fetchall()]
     squashed_data = []
-    for data_points in [data[i :: dims[keys[-1]]] for i in range(dims[keys[-1]])]:
+    for data_points in [data[i :: dim[keys[-1]]] for i in range(dim[keys[-1]])]:
         combined_weight = sum([data_point[1] for data_point in data_points])
         median = (
             0
@@ -68,7 +121,6 @@ def query_data(args, connection, cursor):
             / combined_weight
         )
         squashed_data.append(median)
-    cursor.close()
     return squashed_data
 
 
@@ -94,11 +146,12 @@ def topicmodelling():
     db = get_db()
     cursor = db.cursor()
     if request.method == "GET":
+        schema = "lda_person" if "politicians" in request.args else "lda_group"
         return jsonify(
             {
                 "data": [
                     {"x": 1949 + x, "y": y}
-                    for x, y in enumerate(query_data(request.args, db, cursor))
+                    for x, y in enumerate(query_data(request.args, schema, db, cursor))
                 ]
             }
         )
@@ -107,9 +160,9 @@ def topicmodelling():
 
 @app.teardown_appcontext
 def close_conn(e):
-    db = g.pop('db', None)
+    db = g.pop("db", None)
     if db is not None:
-        app.config['postgreSQL_pool'].putconn(db)
+        app.config["postgreSQL_pool"].putconn(db)
 
 
 if __name__ == "__main__":
