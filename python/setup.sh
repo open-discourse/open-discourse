738d6e3728556490f43177a1eafde118bb7deadb
Jabob Krauskopf <jakob.kraus10@gmail.com>
2022-05-25 16:21:07 +0200

    #99 Added pip upgrade to setup.sh

--- python/setup.sh
+++ python/setup.sh
@@ -3,6 +3,7 @@
 python3 -m venv .venv
 . .venv/bin/activate
 export LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib"
+pip install --upgrade pip
 pip install -r requirements.txt
 cd src/
 pip install -e .
