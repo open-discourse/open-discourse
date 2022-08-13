4a636a75b997dbb37755c8f346d7d79fc5e95921
justyna0330 <justynaklejdysz@gmail.com>
2022-08-13 22:26:13 +0200

    Deleting unnecessary lines missing faction_id

--- python/src/od_lib/08_amendments/01_missing_faction_id.py
+++ python/src/od_lib/08_amendments/01_missing_faction_id.py
@@ -133,30 +133,5 @@ dt = dt.drop([
     'keep_rows'
 ], axis = 1)
 
-dt.to_csv(os.path.join(path_definitions.DATABASE, "speeches_revised.csv"))
+dt.to_csv(os.path.join(path_definitions.DATABASE, "speeches_revised.csv"), index = False)
 
-
-
-dt_noid = speeches[ speeches.politician_id == -1].drop_duplicates(subset = ['first_name', 'last_name', 'electoral_term'])
-dt_names = politicians.drop_duplicates(subset = ['first_name', 'last_name', 'electoral_term'])
-dt_noid['first_name'] = dt_noid.first_name.str.lower()
-dt_noid['last_name']  = dt_noid.last_name.str.lower()
-
-dt_names['first_name_lower'] = politicians['first_name'].str.lower()
-dt_names['last_name_lower'] = politicians['last_name'].str.lower()
-
-dt_noid = dt_noid.merge(dt_names.drop_duplicates(subset = ['first_name','last_name_lower', 'electoral_term']), how = 'left', left_on = ['last_name', 'electoral_term'], right_on = ['last_name_lower', 'electoral_term'])
-matched = dt_noid[~dt_noid.first_name_lower.isnull()]
-matched['dupl'] = matched.duplicated(subset = ['electoral_term','first_name_x', 'last_name_x'], keep = False)
-matched = matched[matched.dupl == False]
-
-matched[(matched.first_name_x.isnull() | ( matched.first_name_lower == matched.first_name_x))].groupby('position_short').count()
-tab = matched[(matched.first_name_x.isnull() | ( matched.first_name_lower == matched.first_name_x))]
-
-check = matched.groupby(['electoral_term', 'session'])['date'].count()
-
-tab.groupby('position_short').count()
-
-test = pd.read_pickle(os.path.join(path_definitions.DATA_FINAL, "speech_content.pkl"))
-test[test.last_name=='Nordt'] # tu jes politician_id: 999990107
-speeches[speeches.last_name=='Nordt'] # tu nie ma
